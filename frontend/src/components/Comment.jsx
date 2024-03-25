79bedf32e2804ce3264faeac057e0b952e79ffce
intbar <tonedead@outlook.com>
2024-03-24 19:39:37 -0500

    added click on username or profile pic in reply to open profile

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,16 +1,21 @@
 import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Comment = ({ reply, lastReply }) => {
 	return (
 		<>
 			<Flex gap={4} py={2} my={2} w={"full"}>
-				<Avatar src={reply.userProfilePic} size={"sm"} />
+				<Link to={`/user/${reply.username}`}>
+					<Avatar src={reply.userProfilePic} size={"sm"} />
+				</Link>
 				<Flex gap={1} w={"full"} flexDirection={"column"}>
-					<Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
-						<Text fontSize='sm' fontWeight='bold'>
-							{reply.username}
-						</Text>
-					</Flex>
+					<Link to={`/user/${reply.username}`}>
+						<Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
+							<Text fontSize='sm' fontWeight='bold'>
+								{reply.username}
+							</Text>
+						</Flex>
+					</Link>
 					<Text>{reply.text}</Text>
 				</Flex>
 			</Flex>
