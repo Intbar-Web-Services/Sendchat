9b5e6e6b32f8c213d890b55e85f2e21efce16b64
intbar <tonedead@outlook.com>
2024-03-25 00:21:20 -0500

    typo ughhh

--- frontend/src/components/Conversation.jsx
+++ frontend/src/components/Conversation.jsx
@@ -43,7 +43,7 @@ const Conversation = ({ conversation, isOnline }) => {
 				})
 			}
 			bg={
-				selectedConversation?._id === conversation._id ? (useColorModeValue("gray.400", "gray.dark")) : ""
+				selectedConversation?._id === conversation._id ? (useColorModeValue("gray.300", "gray.dark")) : ""
 			}
 			borderRadius={"md"}
 		>
