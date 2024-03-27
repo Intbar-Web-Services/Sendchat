ffcba27c8c3d912a334b87c6358a46b895e1050f
intbar <tonedead@outlook.com>
2024-03-27 16:55:27 -0500

    semantics

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,18 +2,14 @@ import { Button, Center, Flex, Image, Link, useColorMode, useColorModeValue, Box
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
-import { AiFillHome } from "react-icons/ai";
 
 import { Portal } from "@chakra-ui/portal";
-import { RxAvatar } from "react-icons/rx";
 import { Link as RouterLink } from "react-router-dom";
 import { useEffect, useCallback } from 'react'
 import { useNavigate } from "react-router-dom";
-import { FiLogOut } from "react-icons/fi";
 import useLogout from "../hooks/useLogout";
 import authScreenAtom from "../atoms/authAtom";
 import { BsFillChatQuoteFill } from "react-icons/bs";
-import { MdOutlineSettings } from "react-icons/md";
 
 const Header = () => {
 	const { colorMode, toggleColorMode } = useColorMode();
