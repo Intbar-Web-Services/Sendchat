5a51e3421dc97ae2c29c4b00ebea18e339eb93ed
intbar <tonedead@outlook.com>
2024-03-27 05:37:14 -0500

    iojrjkkwl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,20 +22,20 @@ function App() {
 	const { socket } = useSocket();
 	console.log(location.pathname);
 
-	function makeNoise() {
-
-	};
-
 	useEffect(() => {
-		if (!location.pathname.includes("chat")) {
-			if (socket) {
-				socket.on("newMessage", () => {
-					const sound = new Audio(messageSound);
-					sound.play();
-				});
+		if (socket) {
+			socket.on("newMessage", () => {
 
-				return () => socket.off("newMessage");
-			}
+				// make a sound if the window is not focused
+				if (location.pathname.includes("chat")) {
+
+					return () => socket.off("newMessage");
+				}
+				const sound = new Audio(messageSound);
+				sound.play();
+			});
+
+			return () => socket.off("newMessage");
 		}
 	}, [socket]);
 
