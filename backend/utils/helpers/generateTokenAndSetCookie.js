009f5c19e28d1082b79717ace6e013be8918b110
intbar <tonedead@outlook.com>
2024-03-29 22:56:21 -0500

    kaljflkdj;lkfjakjfd

--- backend/utils/helpers/generateTokenAndSetCookie.js
+++ backend/utils/helpers/generateTokenAndSetCookie.js
@@ -8,7 +8,6 @@ const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie("jwt", token, {
 		httpOnly: true, // more secure
 		maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-		sameSite: "strict", // CSRF
 	});
 
 	return token;
