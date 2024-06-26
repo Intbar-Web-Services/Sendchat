4d3484c233fc0fd07094e3fa1eb4ea6e3a08e25f
Intbar <tonedead@outlook.com>
2024-06-26 13:42:41 -0500

    fix.

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -56,11 +56,11 @@ async function sendMessage(req, res) {
 
 		let user;
 
-		if (mongoose.Types.ObjectId.isValid(recipientId)) {
-			user = await User.findOne({ _id: recipientId }).select("-password").select("-updatedAt").select("-email");
+		if (mongoose.Types.ObjectId.isValid(senderId)) {
+			user = await User.findOne({ _id: senderId }).select("-password").select("-updatedAt").select("-email");
 		} else {
 			// query is username
-			user = await User.findOne({ username: recipientId }).select("-password").select("-updatedAt").select("-email");
+			user = await User.findOne({ username: senderId }).select("-password").select("-updatedAt").select("-email");
 		}
 
 		let conversation = await Conversation.findOne({
