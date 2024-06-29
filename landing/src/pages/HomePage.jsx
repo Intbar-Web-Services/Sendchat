ac9872b480174766d0c9b227bfe748a4dc8350f8
mercury404 <158371467+RelayMercury403@users.noreply.github.com>
2024-06-29 12:14:47 +0100

    updated landing page

--- landing/src/pages/HomePage.jsx
+++ landing/src/pages/HomePage.jsx
@@ -1,27 +1,26 @@
-import { Divider, Flex, useColorModeValue } from "@chakra-ui/react";
+import { Divider, Flex, Box, Button, Image } from "@chakra-ui/react";
 
 export default function DownloadApp() {
     return (
         <>
-            <h1>Sendchat</h1>
-            <Divider my={4} />
+            <h1 style={{"font-size": 72, "textAlign": "center"}}>Sendchat</h1>
+            <h1 style={{"font-size": 26, "textAlign": "center"}}>The most used least used social media platform on the internet</h1>
+            <Divider my={4}/>
             <h3>
-                <h1>Sendchat is the most used least used social media platform on the internet</h1>
                 With over 1 users, Sendchat is booming in popularity!
                 <br />
                 With over 0 chats sent per day, there is no doubt you will love our &quot;active&quot; chat platform
                 <br />
                 Button which makes the app do applike things
             </h3>
-            <Divider my={4} />
+            <Divider my={4}/>
             <Flex
                 flex='70'
-                bg={useColorModeValue("gray.200", "gray.dark")}
                 borderRadius={"md"}
                 p={2}
                 flexDirection={"column"}
             >
-                <a href="https://app.sendchat.xyz">Open Sendchat</a>
+                <Button onClick={() => { location.href = "https://app.sendchat.xyz" }} position="fixed">Try out Sendchat!</Button>
             </Flex>
         </>
     );
