2d1b9424678d8d0de60cb71039af3debea400962
mercy401 <158371467+mercy401@users.noreply.github.com>
2024-07-07 16:09:11 +0100

    i love giving a cronjob

--- backend/cron/cron.ts
+++ backend/cron/cron.ts
@@ -14,6 +14,16 @@ const job = new cron.CronJob("*/14 * * * *", () => {
 		})
 		.on("error", (e) => {
 			console.error("Error while sending request", e);
+		}); https
+		.get("https://sendchat.xyz", (res: any) => {
+			if (res.statusCode === 200) {
+				console.log("GET request sent successfully");
+			} else {
+				console.log("GET request failed", res.statusCode);
+			}
+		})
+		.on("error", (e) => {
+			console.error("Error while sending request", e);
 		});
 });
 
