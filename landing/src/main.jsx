d1a5dc00009f4ed7018f4869c9f02f845b9da5c3
mercy401 <158371467+mercy401@users.noreply.github.com>
2024-07-07 12:01:50 +0100

    Update main.jsx

--- landing/src/main.jsx
+++ landing/src/main.jsx
@@ -36,6 +36,8 @@ if (navigator.userAgent.includes("sendchat-desktop")) {
 	location.href = "https://app.sendchat.xyz/app";
 }
 
+console.log('STOP POSTING ABOUT SENDCHAT! IM TIRED OF SEEING IT! My friends on TikTok send me memes, on Discord its fucking memes. I was in a server, right, and ALL the channels are just SENDCHAT stuff. I showed my Champion underwear to my girlfriend, and the logo I flipped it and I said "Hey babe, when the underwear SENDCHAT! HAHA! Ding Ding Ding Ding Ding Ding Ding DiDiDing!" I fucking looked at a trash can and I said "Thats a bit SENDCHATsy!" I looked at my penis, I thought of the astronauts helmet and I go "PENIS? MORE LIKE PEN-SENDCHAT!" AAAAAAAAAAAAAA')
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	// React.StrictMode renders every component twice (in the initial render), only in development.
 	<React.StrictMode>
